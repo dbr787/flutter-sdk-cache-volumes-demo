daa370f8c9857e293d9571683350b64c9c534d66
Daniel Ring <dbr787@gmail.com>
2025-07-01 12:19:55 +1000

    Fix setup script to extract Flutter SDK safely into cache volume

--- setup_flutter.sh
+++ setup_flutter.sh
@@ -8,7 +8,7 @@ fi
 
 FLUTTER_ZIP_URL="https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_arm64_${FLUTTER_VERSION}-stable.zip"
 FLUTTER_CACHE_DIR="flutter"
-VERSION_FILE="$FLUTTER_CACHE_DIR/VERSION"
+VERSION_FILE="$FLUTTER_CACHE_DIR/flutter/VERSION"
 TMP_DIR="$(mktemp -d)"
 export PATH="$PWD/$FLUTTER_CACHE_DIR/flutter/bin:$PATH"
 
@@ -18,8 +18,8 @@ else
   echo "⬇️ Installing Flutter $FLUTTER_VERSION..."
   curl -sSL "$FLUTTER_ZIP_URL" -o "$TMP_DIR/flutter_sdk.zip"
   unzip -q "$TMP_DIR/flutter_sdk.zip" -d "$TMP_DIR"
-  rm -rf "$FLUTTER_CACHE_DIR"
-  mv "$TMP_DIR/flutter" "$FLUTTER_CACHE_DIR"
+  rm -rf "$FLUTTER_CACHE_DIR/flutter"
+  mv "$TMP_DIR/flutter" "$FLUTTER_CACHE_DIR/"
 fi
 
 flutter --version
