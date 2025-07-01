28b6afef00e1365e47d73bf81e055bfc2ace22d6
Daniel Ring <dbr787@gmail.com>
2025-07-01 14:22:01 +1000

    Fix PATH export timing in setup_flutter.sh to avoid flutter not found error

--- setup_flutter.sh
+++ setup_flutter.sh
@@ -10,7 +10,6 @@ FLUTTER_ZIP_URL="https://storage.googleapis.com/flutter_infra_release/releases/s
 FLUTTER_CACHE_DIR="flutter"
 VERSION_FILE="$FLUTTER_CACHE_DIR/flutter/VERSION"
 TMP_DIR="$(mktemp -d)"
-export PATH="$PWD/$FLUTTER_CACHE_DIR/flutter/bin:$PATH"
 
 if [ -x "$FLUTTER_CACHE_DIR/flutter/bin/flutter" ] && grep -q "$FLUTTER_VERSION" "$VERSION_FILE"; then
   echo "✅ Flutter $FLUTTER_VERSION is already available in cache"
@@ -21,5 +20,7 @@ else
   rm -rf "$FLUTTER_CACHE_DIR/flutter"
   mv "$TMP_DIR/flutter" "$FLUTTER_CACHE_DIR/"
 fi
+
+export PATH="$PWD/$FLUTTER_CACHE_DIR/flutter/bin:$PATH"
 
 flutter --version
